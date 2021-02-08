fe517d3152935a9d9fc2f931f3a9b77aaa097ffd
camilled671 <72838912+camilled671@users.noreply.github.com>
2021-02-07 21:01:09 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 while True: 
-    print("input.rotation(Rotation.PITCH)" + "input.rotation(Rotation.ROLL)" )
-    if input.rotation(Rotation.PITCH) and input.rotation(Rotation.ROLL) > 4:
+    if input.rotation(Rotation.PITCH) + input.rotation(Rotation.ROLL) > 50:
         music.siren.play_until_done()
     else:
         music.stop_all_sounds()
--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 while (true) {
-    console.log("input.rotation(Rotation.PITCH)" + "input.rotation(Rotation.ROLL)")
-    if (input.rotation(Rotation.Pitch) && input.rotation(Rotation.Roll) > 4) {
+    if (input.rotation(Rotation.Pitch) + input.rotation(Rotation.Roll) > 50) {
         music.siren.playUntilDone()
     } else {
         music.stopAllSounds()
